e26f6fe9e4a52b8d21aa10939240cb6419498eef
EmersonTeles <emersonteles21@gmail.com>
2023-05-06 20:50:02 -0300

    resize gallery on mobile

--- src/components/myGallery.tsx
+++ src/components/myGallery.tsx
@@ -7,20 +7,33 @@ import gallerr05 from "../assets/gallery05.jpg";
 import gallerr06 from "../assets/gallery06.jpeg";
 import gallerr07 from "../assets/gallery07.jpeg";
 import gallerr08 from "../assets/gallery08.jpg";
+import { useEffect, useState } from "react";
 
 export default function MyGallery() {
   const images = [
     { src: gallerr02, aspect_ratio: 9 / 12 },
     { src: gallerr01, aspect_ratio: 9 / 12 },
     { src: gallerr03, aspect_ratio: 9 / 12 },
-    { src: gallerr07, aspect_ratio: 12 / 9 },
     { src: gallerr04, aspect_ratio: 2 / 2 },
+    { src: gallerr07, aspect_ratio: 12 / 9 },
     { src: gallerr06, aspect_ratio: 9 / 12 },
     { src: gallerr05, aspect_ratio: 20 / 9 },
   ];
-  //const widths = [3024, 3024, 3024, 3024];
-  //const ratios = [1, 6, 4, 2];
-  const widths = [1000, 3000, 1000];
-  const ratios = [3, 4, 6, 8];
+  const [width, setWidth] = useState<number | undefined>(undefined);
+
+  let widths = [2000, 3024, 3024];
+  let ratios = [3, 4, 6];
+  useEffect(() => {
+    function handleResize() {
+      setWidth(window.innerWidth);
+    }
+    window.addEventListener("resize", handleResize);
+    handleResize();
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+  if (width && width < 768) {
+    widths = [2000, 3024, 3024];
+    ratios = [2, 4, 6];
+  }
   return <Gallery initState={false} images={images} widths={widths} ratios={ratios} />;
 }
