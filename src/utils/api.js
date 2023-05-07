dc97aec5a45e8d2ec272b069d979d4aca8dc2e24
EmersonTeles <emersonteles21@gmail.com>
2023-05-07 18:37:10 -0300

    changing api route

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.NEXTAUTH_URL || "http://localhost:3000/",
+  baseURL:
+    process.env.NODE_ENV == "production"
+      ? "https://minha-festa.emersonteles.tech"
+      : "http://localhost:3000/",
 });
 export default api;
